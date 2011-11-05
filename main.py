359fcc3d05415bb664097db6f955e017fde8ff76
claudio <claudiomkd@gmail.com>
2011-11-05 17:57:07 +0200

    Add main program logic and callback functionality.
    I have added a simple logic in the main loop to detect the option and perform a
    callback to the corresponding function. The next step is to implement the search
    of products, and other functions to modify the status of the cart (add item,
    remove item, empty cart, etc).

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
-# TODO: headers
+# Valeriy Volodenkov, Eva Rio, Claudio M. Camacho
+# T-121.5300 User Interface Construction
+# Aalto University, School of Science
+import sys
+
+
 
 # keep the number of items in the current cart
 no_items = 0
 
+
+
 # list of actions
-actions = 
-{
+actions = {
     'add_item'    : "Add item",
     'edit_item'   : "Edit item",
     'remove_item' : "Remove item",
     'checkout'    : "Check out",
     'empty_cart'  : "Empty cart",
     'search'      : "Search product",
-    'quit'        : "Quit"
-}
+    'quit'        : "Quit" }
 
 
 
@@ -23,28 +28,45 @@ actions =
 ##
 # Exit the program.
 def quit():
+    print "\nGoodbye!"
     exit()
 
 
 ##
+# Sub-program to help the user find a product.
+def search():
+    print "\nsearch() called successfully!"
+
+
+##
 # Display a list of options and return the selected option.
+#
+# @return the name of the callback to execute.
 def get_menu_option(options):
     count = 1
-    # TODO: dictionary with count+options
     for o in options:
-        print count + ". " + actions[o]
+        print str(count) + ". " + actions[o]
+        count += 1
+
+    return options[int(raw_input("Your choice: ")) - 1];
 
 
 ##
 # Display the initial main menu with the major options to the user.
 def main_menu():
-    print "Items in the cart: " + no_items
-    print "\n"
-    option = get_menu_option('search', 'quit')
+    print "Items in the cart: " + str(no_items) + "\n"
 
+    option = get_menu_option(['search', 'quit'])
+
+    print "\nCallback to execute: " + option
+    return option
 
 
 
 
 # main program
-main_menu()
+opt = ""
+while 1 is 1:
+    opt = main_menu()
+    self_module = sys.modules[__name__]
+    getattr(self_module, opt)()
